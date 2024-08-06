6499aea988f0a1dd804756a0dab7e558c8e04d7e
Oliver Buckley <olibucko20@gmail.com>
2024-08-06 18:02:11 +1000

    Switched MediaPipe framework to GPU inference. Much faster.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import pyglet
 from pyglet import shapes
-
 import pyvolume
 
 def gestures (model: str, num_hands: int, min_hand_detection_confidence: float,
@@ -22,13 +21,13 @@ def gestures (model: str, num_hands: int, min_hand_detection_confidence: float,
   # Variables for recognition logic
   recognition_result_list = []
 
-  def save_result(result: vision.GestureRecognizerResult, unused_output_image: mp.Image,
+  def save_result(result: vision.GestureRecognizerResult, unused_output_image: mp.Image, # type: ignore
                   timestamp_ms: int):
 
     recognition_result_list.append(result)
 
   # Start gesture recognizer
-  base_options = python.BaseOptions(model_asset_path=model)
+  base_options = python.BaseOptions(model_asset_path=model, delegate=python.BaseOptions.Delegate.GPU)
   options = vision.GestureRecognizerOptions(base_options=base_options,
                                           running_mode=vision.RunningMode.LIVE_STREAM,
                                           num_hands=num_hands,
@@ -43,19 +42,17 @@ def gestures (model: str, num_hands: int, min_hand_detection_confidence: float,
   # Continuously capture images from the camera and run inference
   while cap.isOpened():
 
+    # Read frame from camera
     success, image = cap.read()
     if not success:
       sys.exit("ERROR: Cannot read from webcam.")
     
+    # Flip camera to correct orientation
     image = cv2.flip(image, 1)
 
-    # Get dimensions of image
+    # Scale image for improved performance
     h, w = image.shape[:2]
-
-    # Scale them down to 1/2
-    s_h, s_w = int(h/4), int(w/4)
-
-    # Scale the image using 1/2 dimensions
+    s_h, s_w = int(h/2), int(w/2)
     scaled_image = cv2.resize(image, (s_h, s_w), interpolation=cv2.INTER_LINEAR)
 
     # Convert scaled_image to RGB format as needed by MediaPipe framework
@@ -65,47 +62,43 @@ def gestures (model: str, num_hands: int, min_hand_detection_confidence: float,
     # Recognize gestures in the frame
     recognizer.recognize_async(mp_image, time.time_ns() // 1_000_000)
 
-    # If callback from recognizer saved any results (detected hands)
+    # If any hands are detected
     if recognition_result_list:
 
-      # Access landmarks via enumeration. As per MediaPipe spec.
+      # Enumerate detection results and extract information.
       for hand_index, hand_landmarks in enumerate(
         recognition_result_list[0].hand_landmarks):
         
         # Get gesture classification results
         if recognition_result_list[0].gestures:
           gesture = recognition_result_list[0].gestures[hand_index]
+          category_name = ""
           category_name = gesture[0].category_name
           score = round(gesture[0].score, 2)
 
-      
-      # DIRECT REFERENCE: recognition_result_list[0].hand_landmarks[0][0].x -> drop one of the [0] selectors when looping.
-      # DATA FORMAT: NormalizedLandmark(x=value, y=value, z=value, visibility=value, presence=value)
-      # 21 landmarks, 42 total entries for shared array.
-      i = 0
+        # Assign hand coordinates to shared memory array
+        i = 0
+        if hand_landmarks:
+           for landmark in hand_landmarks:
+              array[i] = landmark.x
+              array[i+1] = landmark.y
+              i = i + 2
+
+      # GESTURE CONTROLS DEMO
       try:
-        # Assign landmark coordinates to shared memory array. 
-        for data in recognition_result_list[0].hand_landmarks[0]:
-
-          array[i] = data.x
-          array[i+1] = data.y
-          
-          i = i + 2
-
-        # GESTURE CONTROLS DEMO
-        if category_name == "Closed_Fist":
-           pyvolume.custom(percent=0)         # MUTE CONTROL
-        elif category_name == "Thumb_Up":
-           pyvolume.custom(percent=60)        # TURN VOLUME BACK UP
-
+          if category_name == "Closed_Fist":
+              pyvolume.custom(percent=0)         # MUTE VOLUME
+          elif category_name == "Thumb_Up":
+              pyvolume.custom(percent=60)        # TURN VOLUME BACK UP
+          elif category_name == "None":
+            print("Unrecognized gesture.")
       except:
-        print("No hands in frame.")
+        print("No gesture displayed.")
 
       recognition_result_list.clear()
 
-    # if image is not None:
-    #     cv2.imshow('gesture_recognition', image)          
-
+    if image is not None:
+        cv2.imshow('gesture_recognition', image)          
 
     # Stop the program if the ESC key is pressed.
     if cv2.waitKey(1) == 27:
@@ -118,17 +111,23 @@ def gestures (model: str, num_hands: int, min_hand_detection_confidence: float,
   cv2.destroyAllWindows()
 
 def visualize (array):
-
   # Construct viewing window
   window = pyglet.window.Window(1280, 720)
   
   # Construct graphical batch. For efficient drawing of many shapes.
   batch = pyglet.graphics.Batch()
 
-  # Coordinates dictionary.
+  # Dictionary of coordinates indicators drawn to screen.
   shapes_drawn = {}
 
-  # Populate dictionary. 
+  # Display what laebl is on the screen
+  gesture_label = pyglet.text.Label('Operational.',
+                          font_name='Times New Roman',
+                          font_size=24,
+                          x=window.width-160, y=window.height-40,
+                          anchor_x='center', anchor_y='center')
+
+  # Populate shapes_drawn 
   for i in range(0,21):
       shapes_drawn.update({i:shapes.Circle(x=i + i * 30, y=i + i * 30, radius=5, color=(50, 225, 30), batch=batch)})
 
@@ -144,28 +143,31 @@ def visualize (array):
   def on_draw():
       window.clear()
       batch.draw()
+      gesture_label.draw()
 
   # Event loop
   def update_data(dt):
+    # Get latest coordinates from share array (if any)
+    coordinates = array[:]
 
-          # List of coordinates
-          coordinates = array[:]
+    if coordinates:
+      i = 0
+      j = 1
 
-          i = 0
-          j = 1
+      # Assign new coordinates to drawn shapes
+      for index in shapes_drawn:
+        x = coordinates[i] * scale_factor
+        y = coordinates[j] * scale_factor
 
-          # Assign new coordinates to drawn shapes
-          for index in shapes_drawn:
-            x = coordinates[i] * scale_factor
-            y = coordinates[j] * scale_factor
+        shapes_drawn[index].x = x + x_offset
+        shapes_drawn[index].y = -y + y_offset
 
-            shapes_drawn[index].x = x + x_offset
-            shapes_drawn[index].y = -y + y_offset
+        # Prevent out of range errors
+        if index <= 20:
+          i = i + 2
+          j = j + 2
 
-            # Prevent out of range errors
-            if index <= 20:
-              i = i + 2
-              j = j + 2
+      coordinates = None
 
   pyglet.clock.schedule_interval(update_data, 1/60)
   pyglet.app.run()
